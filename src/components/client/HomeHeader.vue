b1e9870b07e3caf8c5db6db156ee897d5b208944
ahmed-benkrara <ahmed.benkrara12@gmail.com>
2023-05-11 13:23:37 +0200

    finished home header & adding animation on scroll

--- src/components/client/HomeHeader.vue
+++ src/components/client/HomeHeader.vue
@@ -2,17 +2,37 @@
 <div class="w-[calc(100%-20px)] h-[600px] overflow-hidden mx-auto">
     <div class="carousel">
       <div class="carousel-inner">
-        <div class="carousel-item">
-          <img class="w-full h-[600px] object-cover lg:object-top" src="https://images.pexels.com/photos/4872023/pexels-photo-4872023.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="Image 3">
+        <div class="carousel-item relative">
+            <img class="filt w-full scale-x-[-1] h-[600px] object-cover lg:object-top" src="https://images.pexels.com/photos/4872023/pexels-photo-4872023.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="Image 3">
+            <div class="absolute sm:w-full sm:px-2 md:px-0 md:w-fit h-fit sm:left-0 top-0 bottom-0 md:left-[110px] sm:text-center md:text-left m-auto text-white font-poppins font-[300]">
+                <h6 data-aos="zoom-in" data-aos-duration="800" data-aos-easing="ease-in-sine" class="sm:text-[16px] md:text-[18px] mb-[8px] test">Uncover our wide array of packages!</h6>
+                <p data-aos="zoom-in" data-aos-duration="800" data-aos-easing="ease-in-sine" class="md:max-w-[400px] sm:text-[23px] md:text-[25px] font-semibold test">Optimize efficiency effortlessly with our tailored ERP module packages.</p>
+                <router-link data-aos="zoom-in" data-aos-duration="800" data-aos-easing="ease-in-sine" to="/">View More <i class="fa-solid fa-arrow-right-long ml-[4px]"></i></router-link>
+            </div>
         </div>
-        <div class="carousel-item">
-            <img class="w-full h-[600px] object-cover" src="https://images.unsplash.com/photo-1517842536804-bf6629e2c291?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80" alt="Image 1">
+        <div class="carousel-item relative">
+            <img class="filt w-full h-[600px] object-cover" src="https://images.pexels.com/photos/8133879/pexels-photo-8133879.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="Image 2">
+            <div class="absolute sm:w-full sm:px-2 md:px-0 md:w-fit h-fit sm:left-0 top-0 bottom-0 md:left-[110px] sm:text-center md:text-left m-auto text-white font-poppins font-[300]">
+                <h6 data-aos="zoom-in" data-aos-duration="800" data-aos-easing="ease-in-sine"  class="sm:text-[16px] md:text-[18px] mb-[8px] test">Custom ERP Modules: Boost Your Business!</h6>
+                <p data-aos="zoom-in" data-aos-duration="800" data-aos-easing="ease-in-sine"  class="md:max-w-[400px] sm:text-[23px] md:text-[25px] font-semibold test">Enhance Your Business with Customizable ERP Modules. Discover Our Products!</p>
+                <router-link data-aos="zoom-in" data-aos-duration="800"  data-aos-easing="ease-in-sine" to="/">View More <i class="fa-solid fa-arrow-right-long ml-[4px]"></i></router-link>
+            </div>
         </div>
-        <div class="carousel-item">
-            <img class="w-full h-[600px] object-cover" src="https://images.pexels.com/photos/8133879/pexels-photo-8133879.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="Image 2">
+        <div class="carousel-item relative">
+            <img class="filt w-full h-[600px] object-cover lg:object-top" src="https://images.pexels.com/photos/4872020/pexels-photo-4872020.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="Image 2">
+            <div class="absolute sm:w-full sm:px-2 md:px-0 md:w-fit h-fit sm:left-0 top-0 bottom-0 md:left-[110px] sm:text-center md:text-left m-auto text-white font-poppins font-[300]">
+                <h6 data-aos="zoom-in" data-aos-duration="800" data-aos-easing="ease-in-sine" class="sm:text-[16px] md:text-[18px] mb-[8px] test">Your Success is Our Priority!</h6>
+                <p data-aos="zoom-in" data-aos-duration="800" data-aos-easing="ease-in-sine" class="md:max-w-[400px] sm:text-[23px] md:text-[25px] font-semibold test">Unleash Business Excellence with Our Expert Services. Take the First Step Now!</p>
+                <router-link data-aos="zoom-in" data-aos-duration="800"  data-aos-easing="ease-in-sine" to="/">View More <i class="fa-solid fa-arrow-right-long ml-[4px]"></i></router-link>
+            </div>
         </div>
-        <div class="carousel-item">
-          <img class="w-full h-[600px] object-cover lg:object-top" src="https://images.pexels.com/photos/1036622/pexels-photo-1036622.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="Image 2">
+        <div class="carousel-item relative">
+            <img class="filt w-full h-[600px] object-cover lg:object-top" src="https://images.pexels.com/photos/1036622/pexels-photo-1036622.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="Image 2">
+            <div class="absolute sm:w-full sm:px-2 md:px-0 md:w-fit h-fit sm:left-0 top-0 bottom-0 md:left-[110px] sm:text-center md:text-left m-auto text-white font-poppins font-[300]">
+                <h6 data-aos="zoom-in" data-aos-duration="800" data-aos-easing="ease-in-sine" class="sm:text-[16px] md:text-[18px] mb-[8px] test">Connect for Effective Communication!</h6>
+                <p data-aos="zoom-in" data-aos-duration="800" data-aos-easing="ease-in-sine" class="md:max-w-[400px] sm:text-[23px] md:text-[25px] font-semibold test">Unlock Business Excellence with Our Expert Services. Take the First Step Now!</p>
+                <router-link data-aos="zoom-in" data-aos-duration="800" data-aos-easing="ease-in-sine" to="/">View More <i class="fa-solid fa-arrow-right-long ml-[4px]"></i></router-link>
+            </div>
         </div>
       </div>
       <div class="carousel-controls">
@@ -24,6 +44,8 @@
 </template>
 
 <script>
+import AOS from 'aos';
+import 'aos/dist/aos.css';
 
 export default {
     name : 'HomeHeader', 
@@ -54,7 +76,8 @@ export default {
         },
     }, 
     mounted() {
-        //setInterval(this.nextSlide, 5000);
+        setInterval(this.nextSlide, 5000);
+        AOS.init();
     }
 }
 </script>
@@ -73,6 +96,8 @@ export default {
     .carousel-item {
         flex: 0 0 100%;
         width: 100%;
+        background-color: rgba(0, 0, 0, 0.26);
+        display: inline-block;
     }
 
     .carousel-controls {
@@ -80,5 +105,18 @@ export default {
         top: 0;
         right: 0;
         padding: 0.5rem;
+    }
+
+    .test{
+        text-shadow: 1px 1px 2px rgba(0, 0, 0, 0.5);
+    }
+
+    .filt{
+        
+        mix-blend-mode: multiply;
+    }
+
+    .carousel-item a{
+        @apply block md:mx-0 sm:mx-auto w-fit mt-4 text-white border-[2px] sm:text-[11px] md:text-[12px] font-bold font-poppins border-white pl-6 pr-[20px] py-[6px] rounded-full;
     }
 </style>
